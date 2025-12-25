20c34c3b6c27b4d2bf83e443e5d4721f11374f5a
Mikael Hultgren <blomma@artsoftheinsane.com>
2025-12-25 20:04:03 +0100

    Use builtin macros

--- keyboards/splitkb/halcyon/elora/keymaps/fourth/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/fourth/keymap.c
@@ -27,23 +27,6 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_DOT_GRTR]   = ACTION_TAP_DANCE_DOUBLE(KC_DOT, SE_GRTR),
 };
 
-#define _NAV MO(NAV)
-#define _GAME TG(GAME)
-#define _SET TG(SETTING)
-
-#define LGUI_A MT(MOD_LGUI, KC_A)
-#define LALT_S MT(MOD_LALT, KC_S)
-#define LCTL_D MT(MOD_LCTL, KC_D)
-#define LSFT_F MT(MOD_LSFT, KC_F)
-
-#define SYM_SPC LT(SYM, KC_SPACE)
-
-#define RGUI_J MT(MOD_RGUI, KC_J)
-#define RALT_K MT(MOD_RALT, KC_K)
-#define RCTL_L MT(MOD_RCTL, KC_L)
-#define RSFT_O MT(MOD_RSFT, SE_OSLH)
-#define NAV_A LT(NAV, SE_ADIA)
-
 // clang-format off
 // const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
 //   'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
@@ -54,26 +37,26 @@ tap_dance_action_t tap_dance_actions[] = {
 // );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌──────┬────────┬────────┬──────────────────┬────────────────┬──────────────────┐                                  ┌───────────────────┬─────────────────┬────────┬────────┬────────┬───────┐
-//    │ esc  │   1    │   2    │        3         │       4        │        5         │                                  │         6         │        7        │   8    │   9    │   0    │ bspc  │
-//    ├──────┼────────┼────────┼──────────────────┼────────────────┼──────────────────┤                                  ├───────────────────┼─────────────────┼────────┼────────┼────────┼───────┤
-//    │ tab  │   q    │   w    │        e         │       r        │        t         │                                  │         y         │        u        │   i    │   o    │   p    │   å   │
-//    ├──────┼────────┼────────┼──────────────────┼────────────────┼──────────────────┤                                  ├───────────────────┼─────────────────┼────────┼────────┼────────┼───────┤
-//    │ _NAV │ LGUI_A │ LALT_S │      LCTL_D      │     LSFT_F     │        g         │                                  │         h         │     RGUI_J      │ RALT_K │ RCTL_L │ RSFT_O │ NAV_A │
-//    ├──────┼────────┼────────┼──────────────────┼────────────────┼──────────────────┼─────────┬──────┐   ┌───────┬─────┼───────────────────┼─────────────────┼────────┼────────┼────────┼───────┤
-//    │      │   z    │   x    │        c         │       v        │        b         │         │ _SET │   │ _GAME │     │         n         │        m        │   ,    │   .    │        │       │
-//    └──────┴────────┴────────┼──────────────────┼────────────────┼──────────────────┼─────────┼──────┤   ├───────┼─────┼───────────────────┼─────────────────┼────────┼────────┴────────┴───────┘
-//                             │ TD(TD_LCBR_RCBR) │ TD(TD_LPR_RPR) │ TD(TD_LBRC_RBRC) │ SYM_SPC │      │   │       │ ent │ TD(TD_COMMA_LESS) │ TD(TD_DOT_GRTR) │        │
-//    ┌──────┬────────┬────────┼──────────────────┼────────────────┼──────────────────┴─────────┴──────┘   └───────┴─────┴───────────────────┼─────────────────┼────────┼────────┬────────┬───────┐
-//    │  no  │   no   │   no   │        no        │       no       │                                                                         │       no        │   no   │   no   │   no   │  no   │
-//    └──────┴────────┴────────┴──────────────────┴────────────────┘                                                                         └─────────────────┴────────┴────────┴────────┴───────┘
+//    ┌─────────┬───────────┬───────────┬──────────────────┬────────────────┬──────────────────┐                                                 ┌───────────────────┬─────────────────┬───────────┬───────────┬───────────┬────────────┐
+//    │   esc   │     1     │     2     │        3         │       4        │        5         │                                                 │         6         │        7        │     8     │     9     │     0     │    bspc    │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┤                                                 ├───────────────────┼─────────────────┼───────────┼───────────┼───────────┼────────────┤
+//    │   tab   │     q     │     w     │        e         │       r        │        t         │                                                 │         y         │        u        │     i     │     o     │     p     │     å      │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┤                                                 ├───────────────────┼─────────────────┼───────────┼───────────┼───────────┼────────────┤
+//    │ MO(NAV) │ LGUI_T(a) │ LALT_T(s) │    LCTL_T(d)     │   LSFT_T(f)    │        g         │                                                 │         h         │    RGUI_T(j)    │ RALT_T(k) │ RCTL_T(l) │ RSFT_T(ö) │ LT(NAV, ä) │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┼──────────────┬─────────────┐   ┌──────────┬─────┼───────────────────┼─────────────────┼───────────┼───────────┼───────────┼────────────┤
+//    │         │     z     │     x     │        c         │       v        │        b         │              │ TG(SETTING) │   │ TG(GAME) │     │         n         │        m        │     ,     │     .     │           │            │
+//    └─────────┴───────────┴───────────┼──────────────────┼────────────────┼──────────────────┼──────────────┼─────────────┤   ├──────────┼─────┼───────────────────┼─────────────────┼───────────┼───────────┴───────────┴────────────┘
+//                                      │ TD(TD_LCBR_RCBR) │ TD(TD_LPR_RPR) │ TD(TD_LBRC_RBRC) │ LT(SYM, spc) │             │   │          │ ent │ TD(TD_COMMA_LESS) │ TD(TD_DOT_GRTR) │           │
+//    ┌─────────┬───────────┬───────────┼──────────────────┼────────────────┼──────────────────┴──────────────┴─────────────┘   └──────────┴─────┴───────────────────┼─────────────────┼───────────┼───────────┬───────────┬────────────┐
+//    │   no    │    no     │    no     │        no        │       no       │                                                                                        │       no        │    no     │    no     │    no     │     no     │
+//    └─────────┴───────────┴───────────┴──────────────────┴────────────────┘                                                                                        └─────────────────┴───────────┴───────────┴───────────┴────────────┘
 [BASE] = LAYOUT_elora_hlc(
-  KC_ESC  , KC_1   , KC_2   , KC_3             , KC_4           , KC_5             ,                                              KC_6              , KC_7            , KC_8     , KC_9   , KC_0    , KC_BSPC,
-  KC_TAB  , KC_Q   , KC_W   , KC_E             , KC_R           , KC_T             ,                                              KC_Y              , KC_U            , KC_I     , KC_O   , KC_P    , SE_AA  ,
-  _NAV    , LGUI_A , LALT_S , LCTL_D           , LSFT_F         , KC_G             ,                                              KC_H              , RGUI_J          , RALT_K   , RCTL_L , RSFT_O  , NAV_A  ,
-  KC_TRNS , KC_Z   , KC_X   , KC_C             , KC_V           , KC_B             , KC_TRNS , _SET    ,     _GAME   , KC_TRNS  , KC_N              , KC_M            , KC_COMMA , KC_DOT , KC_TRNS , KC_TRNS,
-                              TD(TD_LCBR_RCBR) , TD(TD_LPR_RPR) , TD(TD_LBRC_RBRC) , SYM_SPC , KC_TRNS ,     KC_TRNS , KC_ENTER , TD(TD_COMMA_LESS) , TD(TD_DOT_GRTR) , KC_TRNS                              ,
-  KC_NO   , KC_NO  , KC_NO  , KC_NO            , KC_NO          ,                                                                                     KC_NO           , KC_NO    , KC_NO  , KC_NO   , KC_NO
+  KC_ESC  , KC_1         , KC_2         , KC_3             , KC_4           , KC_5             ,                                                             KC_6              , KC_7            , KC_8         , KC_9         , KC_0            , KC_BSPC         ,
+  KC_TAB  , KC_Q         , KC_W         , KC_E             , KC_R           , KC_T             ,                                                             KC_Y              , KC_U            , KC_I         , KC_O         , KC_P            , SE_AA           ,
+  MO(NAV) , LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D)     , LSFT_T(KC_F)   , KC_G             ,                                                             KC_H              , RGUI_T(KC_J)    , RALT_T(KC_K) , RCTL_T(KC_L) , RSFT_T(SE_OSLH) , LT(NAV, SE_ADIA),
+  KC_TRNS , KC_Z         , KC_X         , KC_C             , KC_V           , KC_B             , KC_TRNS           , TG(SETTING) ,     TG(GAME) , KC_TRNS  , KC_N              , KC_M            , KC_COMMA     , KC_DOT       , KC_TRNS         , KC_TRNS         ,
+                                          TD(TD_LCBR_RCBR) , TD(TD_LPR_RPR) , TD(TD_LBRC_RBRC) , LT(SYM, KC_SPACE) , KC_TRNS     ,     KC_TRNS  , KC_ENTER , TD(TD_COMMA_LESS) , TD(TD_DOT_GRTR) , KC_TRNS                                                         ,
+  KC_NO   , KC_NO        , KC_NO        , KC_NO            , KC_NO          ,                                                                                                    KC_NO           , KC_NO        , KC_NO        , KC_NO           , KC_NO
 ),
 
 //    ┌─────┬─────┬─────┬─────┬─────┬─────┐                           ┌─────┬─────┬─────┬─────┬─────┬─────┐
