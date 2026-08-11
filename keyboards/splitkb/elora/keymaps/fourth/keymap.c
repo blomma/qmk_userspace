afcbac8fecb625cd2675e936787edb554e8e16e9
Mikael Hultgren <blomma@artsoftheinsane.com>
2026-08-11 14:51:58 +0200

    feat(elora): add tap dance brackets and switch to qmk swedish keycodes
    The local i18n.h duplicated keycode definitions that QMK already ships
    in keymap_swedish.h, and its mac-specific brace aliases were the only
    reason it existed. Using the upstream header removes that duplication
    and gives access to SE_ARNG, SE_LABK, SE_QUOT and SE_PIPE, which now
    replace the hand-rolled equivalents on the base and symbol layers.
    
    Braces, parens and brackets move to tap dance keys on the left thumb
    cluster, with comma/less-than and dot/greater-than on the right, so
    TAP_DANCE_ENABLE is turned on via a new rules.mk.
    
    Chordal hold and permissive hold are disabled on both the elora and
    halcyon elora keymaps, and the tapping term returns to the 200ms
    default, since tap dance timing conflicts with those hold heuristics.
    
    The unmodified upstream readme.md, the commented-out mouse key and
    alternate layer blocks, and the halcyon kyria rev4 build target are
    dropped as unused.

--- keyboards/splitkb/elora/keymaps/fourth/keymap.c
+++ keyboards/splitkb/elora/keymaps/fourth/keymap.c
@@ -1,94 +1,82 @@
 #include QMK_KEYBOARD_H
 #include "frames.h"
-#include "i18n.h"
+#include "keymap_swedish.h"
 
 static int num_keypresses = 0;
 static int current_frame  = 0;
 
-enum layers { BASE = 0, SYM, NAV, GAME, SETTING };
+enum layers {
+    BASE = 0,
+    SYM,
+    NAV,
+    GAME,
+    SETTING
+};
 
-//    ┌─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┐       ┌─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┐
-//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
-//    └─────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┴─────┴─────┘
-//                      │     │     │     │     │     │       │     │     │     │     │     │
-//                      └─────┴─────┴─────┴─────┴─────┘       └─────┴─────┴─────┴─────┴─────┘
-// [TEMPLATE] = LAYOUT(
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//                                 KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
-// )
+// Tap Dance declarations
+enum {
+    TD_LCBR_RCBR,
+    TD_LPR_RPR,
+    TD_LBRC_RBRC,
+    TD_COMMA_LESS,
+    TD_DOT_GRTR
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LCBR_RCBR]  = ACTION_TAP_DANCE_DOUBLE(SE_LCBR, SE_RCBR),
+    [TD_LPR_RPR]    = ACTION_TAP_DANCE_DOUBLE(SE_LPRN, SE_RPRN),
+    [TD_LBRC_RBRC]  = ACTION_TAP_DANCE_DOUBLE(SE_LBRC, SE_RBRC),
+    [TD_COMMA_LESS] = ACTION_TAP_DANCE_DOUBLE(SE_COMM, SE_LABK),
+    [TD_DOT_GRTR]   = ACTION_TAP_DANCE_DOUBLE(SE_DOT, SE_RABK),
+};
 
 // clang-format off
-const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
-  'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
-  'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
-  'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
-  'L' , 'L' , 'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R' , 'R' , 'R',
-                    'L' , 'L' , 'L' , 'L' , 'L' ,       'R' , 'R' , 'R' , 'R' , 'R'
-);
-
-#define _NAV    MO(NAV)
-#define _GAME   TG(GAME)
-#define _SET    TG(SETTING)
-
-#define LGUI_A  MT(MOD_LGUI, KC_A)
-#define LALT_S  MT(MOD_LALT, KC_S)
-#define LCTL_D  MT(MOD_LCTL, KC_D)
-#define LSFT_F  MT(MOD_LSFT, KC_F)
-
-#define SYM_SPC LT(SYM, KC_SPACE)
-
-#define RGUI_J  MT(MOD_RGUI, KC_J)
-#define RALT_K  MT(MOD_RALT, KC_K)
-#define RCTL_L  MT(MOD_RCTL, KC_L)
-#define RSFT_O  MT(MOD_RSFT, SE_OSLH)
-#define NAV_A   LT(NAV, SE_ADIA)
+// const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
+//   'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
+//   'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
+//   'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'L' ,       'R' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R',
+//   'L' , 'L' , 'L' , 'L' , 'L' , 'L' , 'L' , 'L' ,       'R' , 'R' , 'R' , 'R' , 'R' , 'R' , 'R' , 'R',
+//                     'L' , 'L' , 'L' , 'L' , 'L' ,       'R' , 'R' , 'R' , 'R' , 'R'
+// );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌──────┬────────┬────────┬────────┬────────┬─────┐         ┌──────┐       ┌───────┐     ┌─────┬────────┬────────┬────────┬────────┬───────┐
-//    │ esc  │   1    │   2    │   3    │   4    │  5  │         │      │       │       │     │  6  │   7    │   8    │   9    │   0    │ bspc  │
-//    ├──────┼────────┼────────┼────────┼────────┼─────┤         ├──────┤       ├───────┤     ├─────┼────────┼────────┼────────┼────────┼───────┤
-//    │ tab  │   q    │   w    │   e    │   r    │  t  │         │      │       │       │     │  y  │   u    │   i    │   o    │   p    │   å   │
-//    ├──────┼────────┼────────┼────────┼────────┼─────┤         ├──────┤       ├───────┤     ├─────┼────────┼────────┼────────┼────────┼───────┤
-//    │ _NAV │ LGUI_A │ LALT_S │ LCTL_D │ LSFT_F │  g  │         │      │       │       │     │  h  │ RGUI_J │ RALT_K │ RCTL_L │ RSFT_O │ NAV_A │
-//    ├──────┼────────┼────────┼────────┼────────┼─────┼─────────┼──────┤       ├───────┼─────┼─────┼────────┼────────┼────────┼────────┼───────┤
-//    │      │   z    │   x    │   c    │   v    │  b  │         │ _SET │       │ _GAME │     │  n  │   m    │   ,    │   .    │        │       │
-//    └──────┴────────┴────────┼────────┼────────┼─────┼─────────┼──────┤       ├───────┼─────┼─────┼────────┼────────┼────────┴────────┴───────┘
-//                             │        │        │     │ SYM_SPC │      │       │       │ ent │     │        │        │
-//                             └────────┴────────┴─────┴─────────┴──────┘       └───────┴─────┴─────┴────────┴────────┘
+//    ┌─────────┬───────────┬───────────┬──────────────────┬────────────────┬──────────────────┐              ┌─────────────┐       ┌──────────┐     ┌───────────────────┬─────────────────┬───────────┬───────────┬─────────────────┬────────────┐
+//    │   esc   │     1     │     2     │        3         │       4        │        5         │              │             │       │          │     │         6         │        7        │     8     │     9     │        0        │    bspc    │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┤              ├─────────────┤       ├──────────┤     ├───────────────────┼─────────────────┼───────────┼───────────┼─────────────────┼────────────┤
+//    │   tab   │     q     │     w     │        e         │       r        │        t         │              │             │       │          │     │         y         │        u        │     i     │     o     │        p        │  SE_ARNG   │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┤              ├─────────────┤       ├──────────┤     ├───────────────────┼─────────────────┼───────────┼───────────┼─────────────────┼────────────┤
+//    │ MO(NAV) │ LGUI_T(a) │ LALT_T(s) │    LCTL_T(d)     │   LSFT_T(f)    │        g         │              │             │       │          │     │         h         │    RGUI_T(j)    │ RALT_T(k) │ RCTL_T(l) │ RSFT_T(SE_ODIA) │ LT(NAV, ä) │
+//    ├─────────┼───────────┼───────────┼──────────────────┼────────────────┼──────────────────┼──────────────┼─────────────┤       ├──────────┼─────┼───────────────────┼─────────────────┼───────────┼───────────┼─────────────────┼────────────┤
+//    │         │     z     │     x     │        c         │       v        │        b         │              │ TG(SETTING) │       │ TG(GAME) │     │         n         │        m        │     ,     │     .     │                 │            │
+//    └─────────┴───────────┴───────────┼──────────────────┼────────────────┼──────────────────┼──────────────┼─────────────┤       ├──────────┼─────┼───────────────────┼─────────────────┼───────────┼───────────┴─────────────────┴────────────┘
+//                                      │ TD(TD_LCBR_RCBR) │ TD(TD_LPR_RPR) │ TD(TD_LBRC_RBRC) │ LT(SYM, spc) │             │       │          │ ent │ TD(TD_COMMA_LESS) │ TD(TD_DOT_GRTR) │           │
+//                                      └──────────────────┴────────────────┴──────────────────┴──────────────┴─────────────┘       └──────────┴─────┴───────────────────┴─────────────────┴───────────┘
 [BASE] = LAYOUT(
-  KC_ESC  , KC_1   , KC_2   , KC_3    , KC_4    , KC_5    ,           KC_TRNS ,         KC_TRNS ,            KC_6    , KC_7    , KC_8     , KC_9   , KC_0    , KC_BSPC,
-  KC_TAB  , KC_Q   , KC_W   , KC_E    , KC_R    , KC_T    ,           KC_TRNS ,         KC_TRNS ,            KC_Y    , KC_U    , KC_I     , KC_O   , KC_P    , SE_AA  ,
-  _NAV    , LGUI_A , LALT_S , LCTL_D  , LSFT_F  , KC_G    ,           KC_TRNS ,         KC_TRNS ,            KC_H    , RGUI_J  , RALT_K   , RCTL_L , RSFT_O  , NAV_A  ,
-  KC_TRNS , KC_Z   , KC_X   , KC_C    , KC_V    , KC_B    , KC_TRNS , _SET    ,         _GAME   , KC_TRNS  , KC_N    , KC_M    , KC_COMMA , KC_DOT , KC_TRNS , KC_TRNS,
-                              KC_TRNS , KC_TRNS , KC_TRNS , SYM_SPC , KC_TRNS ,         KC_TRNS , KC_ENTER , KC_TRNS , KC_TRNS , KC_TRNS
+  KC_ESC  , KC_1         , KC_2         , KC_3             , KC_4           , KC_5             ,                     KC_TRNS     ,         KC_TRNS  ,            KC_6              , KC_7            , KC_8         , KC_9         , KC_0            , KC_BSPC         ,
+  KC_TAB  , KC_Q         , KC_W         , KC_E             , KC_R           , KC_T             ,                     KC_TRNS     ,         KC_TRNS  ,            KC_Y              , KC_U            , KC_I         , KC_O         , KC_P            , SE_ARNG         ,
+  MO(NAV) , LGUI_T(KC_A) , LALT_T(KC_S) , LCTL_T(KC_D)     , LSFT_T(KC_F)   , KC_G             ,                     KC_TRNS     ,         KC_TRNS  ,            KC_H              , RGUI_T(KC_J)    , RALT_T(KC_K) , RCTL_T(KC_L) , RSFT_T(SE_ODIA) , LT(NAV, SE_ADIA),
+  KC_TRNS , KC_Z         , KC_X         , KC_C             , KC_V           , KC_B             , KC_TRNS           , TG(SETTING) ,         TG(GAME) , KC_TRNS  , KC_N              , KC_M            , KC_COMMA     , KC_DOT       , KC_TRNS         , KC_TRNS         ,
+                                          TD(TD_LCBR_RCBR) , TD(TD_LPR_RPR) , TD(TD_LBRC_RBRC) , LT(SYM, KC_SPACE) , KC_TRNS     ,         KC_TRNS  , KC_ENTER , TD(TD_COMMA_LESS) , TD(TD_DOT_GRTR) , KC_TRNS
 ),
 
-//    ┌─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┐       ┌─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┐
-//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │  +  │  {  │  }  │  ´  │  \  │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │  ~  │  (  │  )  │  ^  │  <  │     │
-//    ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-//    │     │     │     │     │     │     │     │     │       │     │     │  -  │  [  │  ]  │  '  │     │     │
-//    └─────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────┴─────┴─────┘
+//    ┌─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┐       ┌─────┐     ┌─────┬─────┬─────┬─────────┬─────────┬─────┐
+//    │     │     │     │     │     │     │     │     │       │     │     │     │     │     │         │         │     │
+//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────────┼─────────┼─────┤
+//    │     │     │     │     │     │     │     │     │       │     │     │  +  │  {  │  }  │    ´    │    \    │     │
+//    ├─────┼─────┼─────┼─────┼─────┼─────┤     ├─────┤       ├─────┤     ├─────┼─────┼─────┼─────────┼─────────┼─────┤
+//    │     │     │     │     │     │     │     │     │       │     │     │  ~  │  (  │  )  │    ^    │ SE_LABK │     │
+//    ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────────┼─────────┼─────┤
+//    │     │     │     │     │     │     │     │     │       │     │     │  -  │  [  │  ]  │ SE_QUOT │ SE_PIPE │     │
+//    └─────┴─────┴─────┼─────┼─────┼─────┼─────┼─────┤       ├─────┼─────┼─────┼─────┼─────┼─────────┴─────────┴─────┘
 //                      │     │     │     │     │     │       │     │     │     │     │     │
 //                      └─────┴─────┴─────┴─────┴─────┘       └─────┴─────┴─────┴─────┴─────┘
 [SYM] = LAYOUT(
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS     , KC_TRNS     , KC_TRNS , KC_TRNS , KC_TRNS,
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_PLUS , SE_LCBR_MAC , SE_RCBR_MAC , SE_ACUT , SE_BSLS , KC_TRNS,
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_TILD , SE_LPRN     , SE_RPRN     , SE_CIRC , SE_LESS , KC_TRNS,
-  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , SE_MINS , SE_LBRC     , SE_RBRC     , SE_APOS , KC_TRNS , KC_TRNS,
-                                KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS     , KC_TRNS
+  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
+  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_PLUS , SE_LCBR , SE_RCBR , SE_ACUT , SE_BSLS , KC_TRNS,
+  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_TILD , SE_LPRN , SE_RPRN , SE_CIRC , SE_LABK , KC_TRNS,
+  KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , SE_MINS , SE_LBRC , SE_RBRC , SE_QUOT , SE_PIPE , KC_TRNS,
+                                KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
 //    ┌─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┐       ┌─────┐     ┌──────┬──────┬─────────┬──────┬─────┬─────┐
@@ -149,23 +137,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-  // QK_RGB_MATRIX_VALUE_UP   , QK_RGB_MATRIX_SPEED_UP   , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                                                 KC_LEFT , KC_DOWN , KC_UP      , KC_RIGHT , KC_TRNS , KC_TRNS,
-  // QK_RGB_MATRIX_VALUE_DOWN , QK_RGB_MATRIX_SPEED_DOWN , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_HOME , KC_PGDN , KC_PAGE_UP , KC_END   , KC_TRNS , KC_TRNS,
-
-// [BASE_OTHER] = LAYOUT(
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS                 , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//                                 KC_TRNS , KC_TRNS , KC_TRNS , LT(SYM_OTHER, KC_SPACE) , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
-// ),
-// [SYM_OTHER] = LAYOUT(
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_PLUS , SE_LCBR , SE_RCBR , SE_ACUT , SE_BSLS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,           KC_TRNS ,         KC_TRNS ,           SE_TILD , SE_LPRN , SE_RPRN , SE_CIRC , SE_LESS , KC_TRNS,
-//   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , SE_MINS , SE_LBRC , SE_RBRC , SE_APOS , KC_TRNS , KC_TRNS,
-//                                 KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
-// ),
 // clang-format on
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -216,7 +187,8 @@ void oled_render_layer_state(void) {
 }
 
 void oled_render_animation_phase(void) {
-    oled_write_raw_P(plant_anim[current_frame], sizeof(plant_anim[current_frame]));
+    oled_write_raw_P(plant_anim[current_frame],
+                     sizeof(plant_anim[current_frame]));
 }
 
 bool oled_task_user(void) {
